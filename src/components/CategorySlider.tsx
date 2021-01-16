5d9a87c32399349028e8aa2844582a0c576757b9
Kolya <nakozlov2018@yandex.ru>
2021-01-17 01:50:05 +0300

    category slider refactoring and some small changes

--- src/components/CategorySlider.tsx
+++ src/components/CategorySlider.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import styles from "./CategorySlider.module.css";
 import ForwardArrow from "../components/svgs/ForwardArrow";
 import BackwardArrow from "../components/svgs/BackwardArrow";
@@ -6,12 +6,39 @@ import classNames from "classnames";
 
 const imgArr = [1, 2, 3, 4, 5, 6, 7];
 
+import React from "react";
+
+const ImageSlide = ({ i }: { i: any }) => {
+  const [showShadow, setShowShadow] = useState(false);
+  return (
+    <a key={i} className={styles.imageWrapper}>
+      <img
+        onMouseEnter={() => setShowShadow(true)}
+        onMouseLeave={() => setShowShadow(false)}
+        className={classNames(styles.image, { [styles.shadow]: showShadow })}
+        src={`/category-slider/${i}.jpg`}
+      />
+      <div
+        className={styles.hl}
+        onMouseEnter={() => setShowShadow(true)}
+        onMouseLeave={() => setShowShadow(false)}
+      />
+      <div
+        className={styles.vl}
+        onMouseEnter={() => setShowShadow(true)}
+        onMouseLeave={() => setShowShadow(false)}
+      />
+      <div className={styles.imageTitle}>{i}</div>
+    </a>
+  );
+};
+
 const CategorySlider = () => {
   const [counter, setCounter] = useState(0);
 
   const style = { transform: `translateX(${-counter * 100}%)` };
   return (
-    <>
+    <div className={styles.wrapper}>
       <h2 className={styles.title}>КАТЕГОРИИ</h2>
       <div className={styles.slider}>
         <BackwardArrow
@@ -36,18 +63,14 @@ const CategorySlider = () => {
             [styles.arrowHidden]: counter > 3,
           })}
         />
-        <div className={styles.sliderWrapper}>
+
+        <div className={styles.sliderWrapper} style={style}>
           {imgArr.map((i) => (
-            <a key={i} className={styles.imageWrapper} style={style}>
-              <img className={styles.image} src={`/category-slider/${i}.jpg`} />
-              <div className={styles.hl} />
-              <div className={styles.vl} />
-              <div className={styles.imageTitle}>{i}</div>
-            </a>
+            <ImageSlide key={i} i={i} />
           ))}
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
