90c4827200f91c92d77d5f790f309be206fb3c81
Kolya <nakozlov2018@yandex.ru>
2021-02-12 18:45:07 +0300

    refactor Sidebar component

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -1,33 +1,31 @@
 import { useState, useContext } from "react";
 import classNames from "classnames";
 import { CategoriesContext } from "../lib/categoryContext";
+import { useRouter } from "next/router";
 import Link from "next/link";
 
 import styles from "./Sidebar.module.css";
 
-interface SidebarProps {
-  categorySlug?: string;
-}
-
-const Sidebar: React.FC<SidebarProps> = ({ categorySlug }) => {
-  const [show, setActive] = useState(false);
+const Sidebar: React.FC = () => {
+  const router = useRouter();
+  const [active, setActive] = useState(false);
   const categories = useContext(CategoriesContext);
 
   return (
     <nav className={styles.sidebar}>
       <h3
-        className={classNames(styles.heading, show ? styles.active : "")}
+        className={classNames(styles.heading, active ? styles.active : "")}
         onClick={() => setActive((a) => !a)}
       >
         КАТАЛОГ
       </h3>
-      <ul className={classNames(styles.list, show ? "" : styles.hidden)}>
+      <ul className={classNames(styles.list, active ? "" : styles.hidden)}>
         {categories.map(({ name, slug }) => (
           <li key={slug} className={styles.item}>
             <Link href={`/catalog/${slug}`}>
               <a
                 className={classNames(styles.link, {
-                  [styles.linkActive]: categorySlug === slug,
+                  [styles.linkActive]: router.query.categorySlug === slug,
                 })}
               >
                 {name}
