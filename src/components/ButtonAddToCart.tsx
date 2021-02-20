06d3423af075d3ec2fd452fba65a923f4073a387
Kolya <nakozlov2018@yandex.ru>
2021-02-20 16:14:00 +0300

    add ButtonAddToCart component

---
+++ src/components/ButtonAddToCart.tsx
@@ -0,0 +1,21 @@
+import { useRouter } from "next/router";
+import Button from "./Button";
+
+interface ButtonAddToCartProps {
+  inCart: boolean;
+  handleAddToCart: () => {};
+}
+
+const ButtonAddToCart: React.FC<ButtonAddToCartProps> = ({
+  inCart,
+  handleAddToCart,
+}) => {
+  const router = useRouter();
+  return inCart ? (
+    <Button onClick={() => router.push("/cart")}>Перейти в корзину</Button>
+  ) : (
+    <Button onClick={handleAddToCart}>Добавить</Button>
+  );
+};
+
+export default ButtonAddToCart;
