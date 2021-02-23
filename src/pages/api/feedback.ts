05c97e3f49dcf8bf563d3a6dfab9060a05929dc2
Kolya <nakozlov2018@yandex.ru>
2021-02-23 21:29:51 +0300

    add endpoint for creating user feedback

---
+++ src/pages/api/feedback.ts
@@ -0,0 +1,21 @@
+import { prisma } from "../../lib/prismaClient";
+import withSession from "../../lib/session";
+
+export default withSession(async (req, res) => {
+  if (req.method === "POST") {
+    const user = req.session.get("user");
+    if (!user || !user.isLogged) {
+      return res.status(401).json({ messsage: "Unauthorized" });
+    }
+    const { productId: productid, comment, rating } = JSON.parse(req.body);
+    try {
+      const feedback = await prisma.feedback.create({
+        data: { comment, rating, productid, userid: user.id },
+      });
+      prisma.$disconnect();
+      return res.json(feedback);
+    } catch {
+      return res.status(400).json({ message: "Bad request" });
+    }
+  }
+});
