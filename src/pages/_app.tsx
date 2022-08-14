0a06638c6b6dca44849a77e3541d4a9f2ac204c1
KozlovNik <nakozlov2018@yandex.ru>
2022-08-14 11:55:22 +0300

    feat: add progress bar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,23 @@
 import type { AppProps } from "next/app";
+import Router from "next/router";
+import NProgress from "nprogress";
 import { ThemeProvider } from "styled-components";
 import { theme } from "../shared/system/theme";
 import "../styles/globals.css";
 import "../../public/main.css";
+import "nprogress/nprogress.css";
+
+NProgress.configure({ easing: "ease", speed: 500 });
+
+Router.events.on("routeChangeStart", () => {
+  NProgress.start();
+});
+
+Router.events.on("routeChangeComplete", () => {
+  NProgress.done();
+});
+
+Router.events.on("routeChangeError", () => NProgress.done());
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
