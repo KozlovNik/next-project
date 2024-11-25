92aa239a8f8be5b5873a64ce88e8631bdd610d3d
Kozlov Nikolay <nakozlov2018@yandex.ru>
2024-11-25 21:28:51 +0300

    style: configure tailwind

--- src/components/FilterForm.tsx
+++ src/components/FilterForm.tsx
@@ -1,13 +1,13 @@
 import { NextRouter } from "next/router";
 import { useContext, useRef, useState } from "react";
-import { Scrollbars } from "react-custom-scrollbars";
+// import { Scrollbars } from "react-custom-scrollbars";
 import qs from "qs";
 import omit from "lodash/omit";
 import useCatalogData from "../hooks/useCatalogData";
 import fetchJson from "../lib/fetchJson";
 import usePath from "../hooks/usePath";
 
-import Thumb from "./Thumb";
+// import Thumb from "./Thumb";
 
 import styles from "./FilterForm.module.css";
 import { LoadingContext } from "../lib/loadingContext";
@@ -30,54 +30,47 @@ const FilterForm: React.FC<FilterFormProps> = ({ render, close, fields }) => {
   const [query, setQuery] = useState(omit(router.query, "categorySlug"));
 
   return (
-    <Scrollbars
-      renderThumbVertical={Thumb}
-      autoHeight
-      autoHeightMin={30}
-      autoHeightMax={180}
-    >
-      <form className={styles.form} ref={formRef}>
-        <div>{render({ query, setQuery })}</div>
-        <div className={styles.buttons}>
-          <button
-            className={styles.reset}
-            onClick={(e) => {
-              e.preventDefault();
-              setQuery(omit(query, fields));
-            }}
-          >
-            Сбросить
-          </button>
-          <button
-            type="submit"
-            className={styles.apply}
-            onClick={async (e) => {
-              const queryString = qs.stringify({ ...query, page: "1" });
-              e.preventDefault();
-              router.push(`${pathname}?${queryString}`, undefined, {
-                shallow: true,
-                scroll: false,
-              });
+    <form className={styles.form} ref={formRef}>
+      <div>{render({ query, setQuery })}</div>
+      <div className={styles.buttons}>
+        <button
+          className={styles.reset}
+          onClick={(e) => {
+            e.preventDefault();
+            setQuery(omit(query, fields));
+          }}
+        >
+          Сбросить
+        </button>
+        <button
+          type="submit"
+          className={styles.apply}
+          onClick={async (e) => {
+            const queryString = qs.stringify({ ...query, page: "1" });
+            e.preventDefault();
+            router.push(`${pathname}?${queryString}`, undefined, {
+              shallow: true,
+              scroll: false,
+            });
 
-              setLoading(true);
-              await mutate(
-                fetchJson(
-                  `/api/products?category=${
-                    router.query.categorySlug || ""
-                  }&${queryString}`,
-                  {}
-                ),
-                false
-              );
-              setLoading(false);
-              close();
-            }}
-          >
-            Применить
-          </button>
-        </div>
-      </form>
-    </Scrollbars>
+            setLoading(true);
+            await mutate(
+              fetchJson(
+                `/api/products?category=${
+                  router.query.categorySlug || ""
+                }&${queryString}`,
+                {}
+              ),
+              false
+            );
+            setLoading(false);
+            close();
+          }}
+        >
+          Применить
+        </button>
+      </div>
+    </form>
   );
 };
 
